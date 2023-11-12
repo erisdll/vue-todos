541828b94508471df69bb26a3766a90a39a71faf
erisdll <72031082+erisdll@users.noreply.github.com>
2023-11-12 18:37:15 -0300

    initial commit

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="../assets/vue-logo.png" alt="" />
+        <h1>Vue Todos</h1>
+      </div>
+      <ul class="nav-routes"></ul>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
